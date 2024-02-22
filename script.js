15084e19df7a8d08142f084c6d831fae9503bd32
kemala4680 <kema20.fathan@gmail.com>
2024-02-22 15:57:20 +0700

    Update 3

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let database = [
   {
+    tag: 1,
     name: 'Rimiru Tempest',
-    telp: '081225234068',
-    area: 'Bekasi'
+    telp: '081292038912',
+    area: 'Bekasi',
+    desc: `I’m not a bad Slime`
+  },
+  {
+    tag: 2,
+    name: 'Alex Bhize',
+    telp: '081265455897',
+    area: 'Jakarta',
+    desc: 'Gas aja lah Bos-ku'
+  },
+  {
+    tag: 3,
+    name: 'Mikasa Ackerman',
+    telp: '082245872563',
+    area: 'Depok',
+    desc: 'Ereeennn'
   }
 ];
 
@@ -10,11 +26,19 @@ function addData() {
   let helperName = document.getElementById("helper-name-input");
   let helperTelp = document.getElementById("helper-telp-input");
   let helperArea = document.getElementById("helper-area-input");
+  let helperDesc = document.getElementById("helper-desc-input");
+
+  let tag = 1;
+  if (database.length > 0) {
+    tag = database[database.length-1].tag + 1;
+  }
 
   let tempObj = {
+    tag,
     name: helperName.value,
     telp: helperTelp.value,
-    area: helperArea.value
+    area: helperArea.value,
+    desc: helperDesc.value
   };
 
   database.push(tempObj);
@@ -22,6 +46,7 @@ function addData() {
   helperName.value = '';
   helperTelp.value = '';
   helperArea.value = '';
+  helperDesc.value = '';
 
   readData();
 }
@@ -31,21 +56,20 @@ function readData(areaInput) {
   for (let i = 0; i < database.length; i++) {
     let perObj = database[i];
 
-    let {name, telp, area} = perObj;
+    let {tag, name, area} = perObj;
 
     if (!areaInput) {
-      template += `<div class="card">
+      template += `<div class="small-card">
           <p>${name}</p>
-          <p>${telp}</p>
           <p>${area}</p>
-          <button>-</button>
+          <button onclick="showMore(${tag})">-</button>
         </div>`
-    } else if (areaInput === area) {
-      template += `<div class="card">
+    } 
+    if (areaInput === area) {
+      template += `<div class="small-card">
           <p>${name}</p>
-          <p>${telp}</p>
           <p>${area}</p>
-          <button>-</button>
+          <button onclick="showMore(${tag},'${area}')">-</button>
         </div>`
     }
   }
@@ -59,13 +83,82 @@ function readData(areaInput) {
 
     nearYouCard.innerHTML = template
   }
-
 }
 
-function nearYou(){
+function nearYou() {
   let userArea = document.getElementById("user-area-input");
 
   readData(userArea.value);
+}
+
+function showMore(tag,areaInput) {
+  let template = '';
+
+  for (let i = 0; i < database.length; i++) {
+    let perObj = database[i];
+
+    let {name, area, telp, desc} = perObj;
+
+    if (!areaInput) {
+      if (perObj.tag === tag) {
+        template += `<div class="big-card">
+              <p>${name}</p>
+              <p>${area}</p>
+              <p>${telp}</p>
+              <p>${desc}</p>
+              <button onclick="removeData(${tag})">delete</button>
+              <button onclick="readData()">-</button>
+            </div>`
+      } else {
+        template += `<div class="small-card">
+            <p>${name}</p>
+            <p>${area}</p>
+            <button onclick="showMore(${tag})">-</button>
+          </div>`
+      }
+    }
+    if (areaInput === area) {
+      if (perObj.tag === tag) {
+        template += `<div class="big-card">
+              <p>${name}</p>
+              <p>${area}</p>
+              <p>${telp}</p>
+              <p>${desc}</p>
+              <button onclick="nearYou()">-</button>
+            </div>`
+      } else {
+        template += `<div class="small-card">
+            <p>${name}</p>
+            <p>${area}</p>
+            <button onclick="showMore(${tag},'${area}')">-</button>
+          </div>`
+      }
+    }
+
+    if (!areaInput) {
+      let helperCard = document.getElementById("helper-card");
+      
+      helperCard.innerHTML = template;
+    } else {
+      let nearYouCard = document.getElementById("near-you-card");
+  
+      nearYouCard.innerHTML = template;
+    }
+  }
+}
+
+function removeData(tag) {
+  if (database.length === 1) {
+    database = [];
+  }
+  for (let i = 0; i < database.length; i++) {
+    let perObj = database[i];
+    if (perObj.tag >= tag) {
+      perObj.tag -= 1;
+    }
+  }
+  database.splice(tag-1,1);
+  readData();
 }
 
 readData();

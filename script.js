58e888625a9dd630f99c5cbe320cffa21c9ee434
andaridea <deapuspita1796@gmail.com>
2024-02-22 17:43:27 +0700

    validasi done

--- script.js
+++ script.js
@@ -6,11 +6,56 @@ let database = [
   }
 ];
 
-function addData() {
+
+function validateAndSubmit() {
   let helperName = document.getElementById("helper-name-input");
   let helperTelp = document.getElementById("helper-telp-input");
   let helperArea = document.getElementById("helper-area-input");
 
+  let helperNameError = document.getElementById("helper-name-error");
+  let helperTelpError = document.getElementById("helper-telp-error");
+  let helperAreaError = document.getElementById("helper-area-error");
+
+  helperNameError.style.display = 'none';
+  helperTelpError.style.display = 'none';
+  helperAreaError.style.display = 'none';
+
+  let isValid = true;
+
+  if (!helperName.value){
+    helperNameError.style.display = 'block';
+    isValid = false;
+  } 
+
+  if(helperTelp.value.length !== 12 || isNaN(helperTelp.value)){
+    helperTelpError.style.display = 'block';
+    isValid = false;
+  }
+
+  let area = ['jakarta', 'bogor', 'depok', 'tangerang', 'bekasi'];
+  let isValidArea = false;
+    for (let x = 0; x < area.length; x++){
+      let perArea = area[x]
+      if(perArea === helperArea.value.toLowerCase()){
+        isValidArea = true;
+        break;
+      }
+      if (!isValidArea){
+        helperAreaError.style.display = 'block';
+        isValid = false;
+      }
+    }
+
+  if(isValid) {
+    addData();
+  }
+}
+
+function addData() {
+  let helperName = document.getElementById("helper-name-input");
+  let helperTelp = document.getElementById("helper-telp-input");
+  let helperArea = document.getElementById("helper-area-input");
+  
   let tempObj = {
     name: helperName.value,
     telp: helperTelp.value,
@@ -25,6 +70,7 @@ function addData() {
 
   readData();
 }
+
 
 function readData(areaInput) {
   let template = '';
